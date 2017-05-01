5d79b11f0c1be7dbabdba57d943a1f726fb6b121
niufennan <jiangchao226@126.com>
2017-05-01 23:38:22 +0800

    创建了Item对象模型和数据库迁移

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
+from lists.models import Item
 from lists.views import home_page
 
 import re
@@ -34,3 +35,22 @@ class HomePageTest(TestCase):
         self.assertIn("A new list item",response.content.decode())
         expected_html=render_to_string("home.html",{"new_item_text":"A new list item"})
         self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_item(self):
+        first_item=Item()
+        first_item.text="The first list item"
+        first_item.save()
+
+        second_item=Item()
+        second_item.text="Item the second"
+        second_item.save()
+
+        saved_item=Item.objects.all()
+        self.assertEqual(saved_item.count(),2)
+
+        first_saved_item=saved_item[0]
+        second_saved_item=saved_item[1]
+
+        self.assertEqual(first_saved_item.text,"The first list item")
+        self.assertEqual(second_saved_item.text,"Item the second")
