fb9f47d97d468583a02cba42b7949b19983ed4ff
niufennan <jiangchao226@126.com>
2017-05-04 00:00:48 +0800

    增加了重定向,并且在模板中显示所有的项

--- lists/tests.py
+++ lists/tests.py
@@ -9,7 +9,6 @@ import re
 
 class HomePageTest(TestCase):
 
-
     @staticmethod
     def remove_csrf(html_code):
         csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
@@ -32,9 +31,38 @@ class HomePageTest(TestCase):
         request.method="POST"
         request.POST["item_text"]="A new list item"
         response=home_page(request)
-        self.assertIn("A new list item",response.content.decode())
-        expected_html=render_to_string("home.html",{"new_item_text":"A new list item"})
-        self.assertEqual(self.remove_csrf(response.content.decode()), self.remove_csrf(expected_html))
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,"A new list item")
+
+
+    def test_home_page_redirects_after_POST(self):
+        request=HttpRequest()
+        request.method="POST"
+        request.POST["item_text"]="A new list item"
+        response=home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
+
+
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        request=HttpRequest()
+        request.method = "POST"
+        request.POST["item_text"] = "A new list item"
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 1)
+
+    def test_home_page_displays_all_list_item(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        request=HttpRequest()
+        response=home_page(request)
+
+        self.assertIn("itemey 1", response.content.decode())
+        self.assertIn("itemey 2", response.content.decode())
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_item(self):
@@ -54,3 +82,4 @@ class ItemModelTest(TestCase):
 
         self.assertEqual(first_saved_item.text,"The first list item")
         self.assertEqual(second_saved_item.text,"Item the second")
+
