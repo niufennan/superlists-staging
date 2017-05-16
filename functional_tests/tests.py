8c1b0679889ebccb7ba5a81af2966603b0368a48
niufennan <jiangchao226@126.com>
2017-05-16 22:18:12 +0800

    修改了database目录的位置

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,27 @@
 from selenium import webdriver
+import sys
 import time
 from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import  StaticLiveServerTestCase
 import unittest
 from selenium.webdriver.common.keys import Keys
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+
+        for arg in sys.argv:
+            if "liveserver" in arg:
+                cls.server_url="http://"+arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url=cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url==cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser=webdriver.Firefox()
         self.browser.implicitly_wait(1)
@@ -13,7 +30,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # 伊迪丝访问首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         #self.browser.set_window_size(1024, 768)
 
         # 她看到输入框完美的居中显示
@@ -29,7 +46,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         #伊迪丝听说有一个很酷的在线代办事项应用
         #她去看了看这个应用的首页
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         #她注意到网页的标题和头部都包含了To-Do这个词
         self.assertIn("To-Do",self.browser.title)
@@ -79,7 +96,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         #弗朗西斯访问首页
         #页面中看不到伊迪丝的清单
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text=self.browser.find_element_by_tag_name("body").text
         self.assertNotIn("购买孔雀羽毛",page_text)
         self.assertNotIn("做假蝇",page_text)
