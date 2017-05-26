96fea0e3cca3234e878e11b02d734f900d6581f4
niufennan <jiangchao226@126.com>
2017-05-26 23:41:49 +0800

    Add gunicorn to virtualenv requirements

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,20 +1,20 @@
 from selenium import webdriver
 import sys
 import time
-from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import  StaticLiveServerTestCase
-import unittest
+
 from selenium.webdriver.common.keys import Keys
+
 class NewVisitorTest(StaticLiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
-
+        super().setUpClass()
         for arg in sys.argv:
             if "liveserver" in arg:
                 cls.server_url="http://"+arg.split('=')[1]
+                #cls.live_server_url=cls.server_url
                 return
-        super().setUpClass()
         cls.server_url=cls.live_server_url
 
     @classmethod
